2320910427a45013b705e2140fbed2d9240a7784
Pau Rosello Van Schoor <paurosello@gmail.com>
2019-06-05 23:30:48 +0200

    Bump version: 0.1.7 → 0.1.8

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # K8Spin CLI
-Version: 0.1.7
+Version: 0.1.8
 
 # Get dependencies
 ```
--- k8spin.go
+++ k8spin.go
@@ -33,7 +33,7 @@ func main() {
 
 	app := cli.NewApp()
 	app.Name = "k8spin"
-	app.Version = "0.1.7"
+	app.Version = "0.1.8"
 	app.Usage = "CLI for managing namespaces"
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
