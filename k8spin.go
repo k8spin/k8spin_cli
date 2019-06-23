e979d430262a78c0cd5fe63fcd548e7eb89ffe34
Pau Rosello Van Schoor <paurosello@gmail.com>
2019-06-23 23:00:01 +0200

    Bump version: 0.1.21 → 0.1.22

--- k8spin.go
+++ k8spin.go
@@ -42,7 +42,7 @@ func main() {
 
 	app := cli.NewApp()
 	app.Name = "k8spin"
-	app.Version = "0.1.21"
+	app.Version = "0.1.22"
 	app.Usage = "CLI for managing namespaces"
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
